00be51dd0660a3d4748a14f7ccb7c4316ecb6e44
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-11-27 15:50:30 +0000

    displaying score in tictactoe

--- src/TicTacToe/TicTacToe.cpp
+++ src/TicTacToe/TicTacToe.cpp
@@ -9,6 +9,7 @@ const int board_size = 3;
 #define O_WON_STATE 2
 #define TIE_STATE 3
 #define QUIT_STATE 4
+#define DISPLAY_SCORE_STATE 5
 
 #define EMPTY 0
 #define PLAYER_X 1
@@ -24,6 +25,8 @@ static const SDL_Color PLAYER_X_COLOUR = {.r = 255, .g = 50, .b = 50};
 static const SDL_Color PLAYER_O_COLOUR = {.r = 50, .g = 100, .b = 255};
 static const SDL_Color TIE_COLOUR = {.r = 100, .g = 100, .b = 100};
 
+static const SDL_Color TEXT_COLOUR_WHITE = {255, 255, 255};
+
 const int cell_width = 213;
 const int cell_height = 160;
 
@@ -38,7 +41,9 @@ private:
     } game_t;
 
     game_t m_game;
-    SDL_Event event;
+
+    int x_win_counter;
+    int o_win_counter;
 
 public:
     virtual bool OnUserCreate()
@@ -49,6 +54,9 @@ public:
                       EMPTY, EMPTY, EMPTY},
             .player = PLAYER_X,
             .state = RUNNING_STATE};
+
+        x_win_counter = 0;
+        o_win_counter = 0;
 
         return true;
     }
@@ -123,10 +131,12 @@ private:
         if (Check_player_won(PLAYER_X))
         {
             m_game.state = X_WON_STATE;
+            x_win_counter++;
         }
         else if (Check_player_won(PLAYER_O))
         {
             m_game.state = O_WON_STATE;
+            o_win_counter++;
         }
         else if (Count_cells(m_game.board, EMPTY) == 0)
         {
@@ -158,13 +168,20 @@ private:
 
     void Click_on_cell(int row, int column)
     {
-        if (m_game.state == RUNNING_STATE)
+        switch (m_game.state)
         {
+        case RUNNING_STATE:
             Player_turn(row, column);
-        }
-        else
+            break;
+        case X_WON_STATE:
+        case O_WON_STATE:
+        case TIE_STATE:
+            m_game.state = DISPLAY_SCORE_STATE;
+            break;
+        default:
         {
             Reset_game();
+        }
         }
     }
     void Render_X(int row, int column, const SDL_Color *color)
@@ -268,6 +285,43 @@ private:
         Render_board(colour, colour);
     }
 
+    void Render_score()
+    {
+        TTF_Font *font = TTF_OpenFont("..\\res\\fonts\\RobotoMono-Bold.ttf", 32);
+        if (!font)
+        {
+            fprintf(stderr, "Error using TTF_OpenFont: %s\n", TTF_GetError());
+            return;
+        }
+
+        char char_score[10];
+        sprintf(char_score, "%d - %d", x_win_counter, o_win_counter);
+
+        SDL_Surface *surface_score = TTF_RenderText_Blended(font, char_score, TEXT_COLOUR_WHITE);
+        if (!surface_score)
+        {
+            fprintf(stderr, "Error using TTF_RenderText_Blended: %s\n", TTF_GetError());
+            return;
+        }
+        SDL_Texture *texture_score = SDL_CreateTextureFromSurface(GetRenderer(), surface_score);
+
+        // center text on screen
+        const int x_pos = (SCREEN_WIDTH / 2) - (surface_score->w / 2);
+        const int y_pos = (SCREEN_HEIGHT / 2) - (surface_score->h / 2);
+        SDL_Rect score_rect_dest;             // create a rect
+        score_rect_dest.x = x_pos;            // controls the rect's x coordinate
+        score_rect_dest.y = y_pos;            // controls the rect's y coordinte
+        score_rect_dest.w = surface_score->w; // controls the width of the rect
+        score_rect_dest.h = surface_score->h; // controls the height of the rect
+
+        SDL_RenderCopy(GetRenderer(), texture_score, NULL, &score_rect_dest);
+
+        // Don't forget to free the surface and texture
+        TTF_CloseFont(font);
+        SDL_FreeSurface(surface_score);
+        SDL_DestroyTexture(texture_score);
+    }
+
     void Render_game()
     {
         switch (m_game.state)
@@ -283,6 +337,9 @@ private:
             break;
         case TIE_STATE:
             Render_game_over_state(&TIE_COLOUR);
+            break;
+        case DISPLAY_SCORE_STATE:
+            Render_score();
             break;
         default:
             break;
